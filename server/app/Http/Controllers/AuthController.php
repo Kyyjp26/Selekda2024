72d259d7594d94a6a41b81f59eeec51024603fcb
Kyyjp26 <kyy.prtma26@gmail.com>
2024-08-30 20:30:17 +0700

    fix

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -79,7 +79,7 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'message' => 'Login successfully',
+            'message' => 'login success',
             'token' =>  $token
         ], 200);
     }
