a38f3c34a20dcd7e835e21a3283da4d7d6bc4144
Kyyjp26 <kyy.prtma26@gmail.com>
2024-08-30 07:54:24 +0700

    User

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'username' => 'required|unique:users',
+            'email' => 'required|unique:users',
+            'password' => 'required',
+            'birth_date' => 'required|date',
+            'phone_number' => 'required',
+            'profile_picture' => 'nullable|image|max:2048',
+            'date' => 'nullable|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'invalid register',
+                'erorrs' => $validator->errors()
+            ],422);
+        }
+
+        $fotoPath = null;
+
+        if ($request->hasFile('profile_picture')) {
+            $foto = $request->file('profile_picture');
+            $base64Image = base64_encode(file_get_contents($foto->path()));
+            $fotoPath = $base64Image;
+        }
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'username' => $request->input('username'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'birth_date' => $request->input('birth_date'),
+            'phone_number' => $request->input('phone_number'),
+            'profile_picture' => $fotoPath,
+            'date' => now()
+        ]);
+
+        return response()->json([
+            'message' => 'login sucess',
+            'data' => $user
+        ],201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'invalid login'
+            ],401);
+        }
+
+        $validated = $validator->safe()->only(['username', 'password']);
+        if (!Auth::attempt($validated)) {
+            return response()->json([
+                'message' => 'invalid login'
+            ],401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successfully',
+            'token' =>  $token
+        ], 200);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'message' => 'logout success'
+        ], 200);
+    }
+}
--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->date('birth_date');
+            $table->integer('phone_number');
+            $table->longText('profile_picture');
+            $table->date('date');
             $table->timestamps();
         });
 
